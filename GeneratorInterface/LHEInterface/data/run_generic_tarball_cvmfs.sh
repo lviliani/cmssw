e2e914676faebfbe0ab185a6787b25e35fc1a289
Lorenzo Viliani <lorenzo.viliani@cern.ch>
2026-07-23 17:34:53 +0200

    Upgrade run_generic_tarball_cvmfs.sh for ARM gridpacks

--- GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh
+++ GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #script to run generic lhe generation tarballs
 #kept as simply as possible to minimize need
@@ -25,6 +25,19 @@ echo "%MSG-MG5 random seed used for the run = $rnum"
 
 ncpu=${4}
 echo "%MSG-MG5 thread count requested = $ncpu"
+
+use_singularity=0
+
+for arg in "${@:5}"; do
+  if [ "$arg" = "use_singularity" ]; then
+      use_singularity=1
+  else
+      extra_args+=("$arg")
+  fi
+done
+
+# Rebuild all positional parameters excluding the use_singularity keyword
+set -- "$path" "$nevt" "$rnum" "$ncpu" "${extra_args[@]}"
 
 echo "%MSG-MG5 residual/optional arguments = ${@:5}"
 
@@ -54,6 +67,47 @@ if [ "$use_gridpack_env" = false -a -n "$scram_arch_version" -a -n  "$cmssw_vers
   cd $LHEWORKDIR
 fi
 
+# we should now have SCRAM_ARCH defined, check if gridpack has /SCRAM_ARCH/:
+if [[ ${path} == */SCRAM_ARCH/* ]]; then
+   echo "Gridpack with heterogeneous architecture support"
+   gridpath="${path/\/SCRAM_ARCH\//"/$SCRAM_ARCH/"}"
+   if [ ! -f "$gridpath" ]; then
+      echo "No gridpack for ${SCRAM_ARCH}"
+      tmparch="${SCRAM_ARCH#*_}"
+      subarch="*_${tmparch%_*}_*"
+      gridpaths=`ls -1 ${path/\/SCRAM_ARCH\//"/$subarch/"} 2>/dev/null`
+      if [ -z "$gridpaths" ]; then
+         echo "No gridpack for ${subarch}, exiting"
+         exit 1
+      else
+         scram_osys="${SCRAM_ARCH%%_*}"
+         scram_osno=`echo "$scram_osys" | grep -Eo "[0-9]*"`
+         scram_comp="${SCRAM_ARCH##*_}"
+         scram_cmpn=`echo "$scram_comp" | grep -Eo "[0-9]*"`
+         gridstem="${path%%SCRAM_ARCH/*}"
+         for gridpath in $gridpaths; do
+            gridarch=`echo "${gridpath##$gridstem}" | cut -d/ -f1`
+            gridosys="${gridarch%%_*}"
+            gridosno=`echo "$gridosys" | grep -Eo "[0-9]*"`
+            gridcomp="${gridarch##*_}"
+            gridcmpn=`echo "$gridcomp" | grep -Eo "[0-9]*"`
+            if [ ${gridosno:-0} -le ${scram_osno:-0} ] && \
+               [ ${gridcmpn:-0} -le ${scram_cmpn:-0} ]; then
+                  echo "Using ${gridarch}"
+                  break
+            else
+               echo "Inappropriate ${gridarch}"
+            fi
+         done
+      fi
+   else
+      echo "Using ${SCRAM_ARCH}"
+   fi
+else
+   echo "Mono-architecture gridpack"
+   gridpath=$path
+fi
+
 if [[ -d lheevent ]]
     then
     echo 'lheevent directory found'
@@ -63,14 +117,31 @@ fi
 mkdir lheevent; cd lheevent
 
 #untar the tarball directly from cvmfs
-tar -xaf ${path} 
+tar -xaf ${gridpath} 
 
 # If TMPDIR is unset, set it to the condor scratch area if present
 # and fallback to /tmp
 export TMPDIR=${TMPDIR:-${_CONDOR_SCRATCH_DIR:-/tmp}}
 
+# define singularity
+if [ "$use_gridpack_env" != false ]; then
+    if [ -n "$scram_arch_version" ]; then
+        sing=$(echo ${scram_arch_version} | sed -E 's/^[^0-9]*([0-9]{1,2}).*/\1/')
+    elif egrep -q "scram_arch_version=[^$]" runcmsgrid.sh; then
+        sing=$(grep "scram_arch_version=[^$]" runcmsgrid.sh | sed -E 's/^[^0-9]*([0-9]{1,2}).*/\1/')
+    fi
+    if [ -n "${sing}" ]; then
+        sing="cmssw-el"${sing}" --"
+    fi
+fi
+
 #generate events
-./runcmsgrid.sh $nevt $rnum $ncpu ${@:5}
+if [ "$use_singularity" -eq 1 ]; then
+    echo "Using singularity for running events"
+    ${sing} ./runcmsgrid.sh $nevt $rnum $ncpu ${@:5}
+else
+    ./runcmsgrid.sh $nevt $rnum $ncpu ${@:5}
+fi
 
 mv cmsgrid_final.lhe $LHEWORKDIR/
 
